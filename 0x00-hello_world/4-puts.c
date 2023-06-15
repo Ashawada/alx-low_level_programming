28cd30798b8c4fb1cfd8fa14799cda0eab60a2db
Ashawada <132355866+Ashawada@users.noreply.github.com>
2023-06-15 11:20:24 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/*
+* main - using "main" function , liberary "stdio"
+* this program prints "Programming is like building a multilingual puzzle using puts"
+* Return: 0
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
