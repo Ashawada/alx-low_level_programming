a30df2730b8abf70519c7c8d87af9204ace2d7e4
Ashawada <132355866+Ashawada@users.noreply.github.com>
2023-06-16 10:53:33 -0700

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -16,13 +16,13 @@ int main(void)
 	ch = 'a';
 	while (ch <= 'z')
 	{
-	printf("%c", ch);
+	putchar(ch);
 	ch++;
 	}
 	ch = 'A';
 	while (ch <= 'Z')
 	{
-	printf("%c", ch);
+	putchar(ch);
 	ch++;
 	}
 	putchar('\n');
