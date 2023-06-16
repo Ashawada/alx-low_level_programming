e3367af5a90e9f2c43fa36f5787daa6d09db86f8
Ashawada <132355866+Ashawada@users.noreply.github.com>
2023-06-16 11:43:58 -0700

    9. Patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: 'prints all the numbers of base 16 in lowercase'
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int num;
+
+	num = '0';
+	while (num <= '9')
+	{
+	putchar(num);
+	putchar(',');
+	putchar(' ');
+	num++;
+	}
+	putchar('\n');
+	return (0);
+}
