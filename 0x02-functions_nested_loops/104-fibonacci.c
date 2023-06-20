098bb899003776937db397f8bd648e9c358fe145
Ashawada <132355866+Ashawada@users.noreply.github.com>
2023-06-20 16:58:08 -0700

    16. In computer class

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,7 +11,6 @@ int main(void)
 	unsigned long int i, j, k, j1, j2, k1, k2;
 
 	j = 1;
-
 	k = 2;
 
 	printf("%lu", j);
@@ -22,6 +21,7 @@ int main(void)
 		k = k + j;
 		j = k - j;
 	}
+
 	j1 = j / 1000000000;
 	j2 = j % 1000000000;
 	k1 = k / 1000000000;
@@ -37,4 +37,6 @@ int main(void)
 		j2 = k2 - j2;
 	}
 	printf("\n");
+
 	return (0);
+}
