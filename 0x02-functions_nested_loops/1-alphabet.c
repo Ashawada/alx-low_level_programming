56980b4266c63eaadbf33a7879e95706678d63a9
Ashawada <132355866+Ashawada@users.noreply.github.com>
2023-06-20 13:15:46 -0700

    1. I sometimes suffer from insomnia.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* print_alphabet - 'Printing alphabet using putchar prototype'
+*
+*/
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	_putchar(ch);
+	_putchar('\n');
+}
