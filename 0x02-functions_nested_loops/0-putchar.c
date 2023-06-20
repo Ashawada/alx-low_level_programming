2f2e49e7701222fea5d2202f62071a512d123983
Ashawada <132355866+Ashawada@users.noreply.github.com>
2023-06-20 13:00:49 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
 * main - Entry point
 *
-* Description: 'Print _putchar'
+* Description: 'Printing _putchar using putchar prototype'
 *
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-printf("_putchar\n");
-return (0);
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+	_putchar(str[ch]);
+	_putchar('\n');
+	return (0);
 }
