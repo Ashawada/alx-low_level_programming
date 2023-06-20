8a17bd9ee91e71587fe782a414d244ccf4af20d7
Ashawada <132355866+Ashawada@users.noreply.github.com>
2023-06-20 16:20:45 -0700

    13. Nature made the natural numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,5 +1,4 @@
-#include <main.h>
-#include <stdlib.h>
+#include "main.h"
 
 /**
 *main - prints count of multiples of 3 or 5 below 1024
@@ -12,11 +11,10 @@ int main(void)
 	int total, num
 
 	for (num = 0; num < 1024; num++)
-    {
+	{
 		if ((num % 3 == 0) || (num % 5 == 0))
-        	sum +=num;
+			sum += num;
 	}
 	printf("%d\n", sum);
-
 	return (0);
 }
