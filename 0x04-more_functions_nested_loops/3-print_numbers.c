c8abfc480b5bf77da504cb96964507d1afe34660
Ashawada <132355866+Ashawada@users.noreply.github.com>
2023-06-22 16:59:42 -0700

    3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * mul -  multiplies two integers.
+ * @a: first integer
+ * @b: 2nd integer
+ * Return:  result
+*/
+
+void print_numbers(void)
+
+{
+	int i=0;
+	while (i <= 9)
+	{
+	_putchar(i + 48);
+	i++;
+	}
+	_putchar("\n");
+}
