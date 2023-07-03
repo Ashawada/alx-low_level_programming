222746a4452781b60e6d8b257b9de893281cc51a
Ashawada <132355866+Ashawada@users.noreply.github.com>
2023-07-03 10:59:54 -0700

    1. memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,13 +8,14 @@
 *
 * Return: pointer to the memory area dest
 */
-char *_memcpy(char *dest, char *src, unsigned int n);
+
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
-		dest[i] = scr[i];
+		dest[i] = src[i];
 	}
 
 	return (dest);
